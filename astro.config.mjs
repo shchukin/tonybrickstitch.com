113443a626c258573adba8e7d5a82154d6a952c8
Anton Shchukin <a.a.shchukin@gmail.com>
2026-08-03 01:58:58 +0700

    build

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,4 +3,7 @@ import { defineConfig } from 'astro/config';
 export default defineConfig({
     site: 'https://tonybrickstitch.com',
     outDir: 'docs',
+    build: {
+        inlineStylesheets: 'always',
+    }
 });
